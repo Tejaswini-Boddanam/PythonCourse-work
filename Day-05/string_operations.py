5841d07ba58e38bda624f6d18f7c5ba390f06299
Tejaswini-Boddanam <tejaswiniboddanam@gmail.com>
2026-09-05 15:55:05 +0530

    string operations

---
+++ Day-05/string_operations.py
@@ -0,0 +1,54 @@
+
+s=''
+s
+''
+s='codegnan'
+s
+'codegnan'
+'codegnan'+'pfs'
+... 'codegnanpfs'
+... 'codegnan'*10
+... 'codegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnan'
+... '*' * 10
+... '**********'
+... '-' *5
+... '-----'
+... >>> s1='reena'
+... >>> s1[0]
+... 'r'
+... >>> s1[-1]
+... 'a'
+... >>> s1[-3]
+... 'e'
+... >>> s1[3]
+... 'n'
+... >>> s1='reena','kowsar','nandu'
+... >>> s1
+... ('reena', 'kowsar', 'nandu')
+... >>> s1[0]
+... 'reena'
+... >>> s1[-1]
+... 'nandu'
+... >>> s1='reena kowsar nandu'
+... >>> s1
+... 'reena kowsar nandu'
+... >>> s1[0]
+... 'r'
+... >>> s1[:5]
+... 'reena'
+... >>> s1[-1:-6:-1]
+... 'udnan'
+... >>> s1[12:]
+... ' nandu'
+... >>> s1[::-1]
+... 'udnan raswok aneer'
+... >>> s1[-12::-1]
+... 'k aneer'
+... >>> s1[-13:-1]
+... ' kowsar nand'
+... >>> s1[-13::-1]
+... ' aneer'
+... >>> 'reena' in s1
+... True
+... >>> 'star' in s1
+... False
