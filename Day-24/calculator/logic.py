325438a2cc42e7f0c545f5f9c93de407bf1b4a07
Tejaswini-Boddanam <tejaswiniboddanam@gmail.com>
2026-09-05 16:22:40 +0530

    ATM- logic,main & calculator-login & main

---
+++ Day-24/calculator/logic.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    print("Addition:",a+b)
+
+def sub(a,b):
+    print("subtraction:",a-b)
+
+def mul(a,b):
+    print("mul:",a*b)
+
+def div(a,b):
+    print("Div:",a/b)
+
+def rem(a,b):
+    print("rem:",a%b)
+
+def exp(a,b):
+    print("exp:",a**b)
+
+
---
+++ Day-24/calculator/main.py
@@ -0,0 +1,25 @@
+'''
+import logic
+
+logic.add(2,3)
+logic.sub(2,3)
+logic.mul(2,3)
+logic.div(2,3)
+logic.rem(2,3)
+logic.exp(2,3)
+'''
+
+'''from logic import add,sub
+
+add(2,3)
+sub(4,2)
+'''
+
+from logic import*
+
+add(2,3)
+sub(2,3)
+mul(2,3)
+div(2,3)
+rem(2,3)
+exp(2,3)
